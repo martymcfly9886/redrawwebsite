510b979379f131f0cce717702d967a215c554843
martymcfly9886 <121950258+martymcfly9886@users.noreply.github.com>
2023-01-04 18:42:14 +0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const navMenu = document.querySelector('.nav-menu');
-const burger = document.querySelector('.burger');
-
-burger.addEventListener('.click', () => {
-    navMenu.classList.toggle('active');
-    burger.classList.toggle('active');
-})
